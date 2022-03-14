7eb3d99f64c477b6df26fef95d58700412fc6d28
arthur-raizberg <arthur.raizberg@plus500.com>
2022-03-14 20:45:50 +0200

    Sort local DB movies

--- theater-node-rest-api/controllers/internal-movies.controller.js
+++ theater-node-rest-api/controllers/internal-movies.controller.js
@@ -6,31 +6,40 @@ module.exports = {
       if (error) {
         return next(error);
       } else {
-        res.status(200).json(movies);
+        const sortedMovies = movies.sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        });
+        res.status(200).json(sortedMovies);
       }
     });
   },
 
   saveMovie: (req, res, next) => {
-    const query = {movieId: req.params.movieId};
-    const options = { upsert: true, new: true};
-    Movie.findOneAndUpdate(query, req.body, options, function(error, result) {
+    const query = { movieId: req.params.movieId };
+    const options = { upsert: true, new: true };
+    Movie.findOneAndUpdate(query, req.body, options, function (error, result) {
       if (error) {
         return next(error);
       } else {
         res.status(200).json(result);
       }
-  });
+    });
   },
 
   deleteMovie: (req, res, next) => {
-    Movie.findOneAndDelete({movieId: req.params.movieId}, (error, result) => {
+    Movie.findOneAndDelete({ movieId: req.params.movieId }, (error, result) => {
       if (error) {
         return next(error);
       } else {
         res.status(200).json(result);
       }
-    })
+    });
   },
 
   getMovie: (req, res, next) => {
